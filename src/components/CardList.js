4f251283003610b3b2f6fa7a24a291f41ecd030a
omg3011 <joeljoel3011@gmail.com>
2021-04-30 14:05:29 +0800

    Second Commit

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
-    
     const cardArray = robots.map((user) => {
         return <Card key={user.id} id={user.id} name={user.name} email={user.email}/>;
     });
